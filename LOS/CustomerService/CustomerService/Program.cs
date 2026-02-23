3c3f6db6d6446cf384aa93f6f0fdc42b43a8e2e4
nupur <nupurkarpe47@gmail.com>
2026-02-23 17:02:18 +0530

    CUSTOMER SERVICE

--- LOS/CustomerService/CustomerService/Program.cs
+++ LOS/CustomerService/CustomerService/Program.cs
@@ -22,7 +22,7 @@ builder.Logging.AddConsole();
 
 builder.Services.AddHttpClient<UserServiceClient>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["AuthAddress"] ?? "http://localhost:5266");
+    client.BaseAddress = new Uri(builder.Configuration["AuthAddress"] ?? "loanauth-fxafbnechyf7e2dm.canadacentral-01.azurewebsites.net");
 });
 
 
